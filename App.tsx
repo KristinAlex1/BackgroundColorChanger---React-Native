78c200063e55ba01908997dafd948b248d6bb390
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-14 21:53:25 -0400

    Added few shapes

--- App.tsx
+++ App.tsx
@@ -4,16 +4,29 @@ import React, { useState } from 'react'
 export default function App() {
 
   const [randombackgroundColor, setrandomBackgroundColor] = useState('#000000')
+  const [randomCircleColor, setRandomCircleColor] = useState('black')
+  const [randomCircleColor1, setRandomCircleColor1] = useState('black')
 
   const generateColor = ()=> {
     let color = '#';
+    let color1 = '#';
+    let color2 = '#';
+    let color3 = '#';
     let values = '1234567890ABCDEF';
 
     for (let i =0 ; i<6; i++){
-      color += values[Math.floor(Math.random() * 16)]
+      color += values[Math.floor(Math.random() * 16)];
+
+      color1 += values[Math.floor(Math.random() * 16)];
+      color2 += values[Math.floor(Math.random() * 16)];
+      color3 += values[Math.floor(Math.random() * 16)];
 
     }
+
+    
     setrandomBackgroundColor(color);
+    setRandomCircleColor(color1);
+    setRandomCircleColor1(color2);
 
   }
 
@@ -21,6 +34,10 @@ export default function App() {
     <>
     <StatusBar backgroundColor={randombackgroundColor}/>
     <View style = {[styles.container, {backgroundColor: randombackgroundColor} ]}>
+      <View style = {[styles.circle, {backgroundColor : randomCircleColor}]}/>
+      <View style = {[styles.circle, {backgroundColor : randomCircleColor1}]}/>
+
+    
       <TouchableOpacity onPress={generateColor}>
         <View style = {styles.button}>
           <Text style = {styles.buttonTxt}>Press me</Text>
@@ -43,12 +60,21 @@ const styles = StyleSheet.create({
   button : {
     
     height: 50,
-    width: 100,
+    width: 130,
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderRadius: 20
   },
   buttonTxt : {
+    fontSize: 20
+  },
+  circle : {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginVertical: 30,
+    marginHorizontal: 55
     
   }
 })
